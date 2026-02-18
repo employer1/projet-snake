2c341ecba836dcb514c4dce1a9e140bf32f20106
Hawai360 <47851295+employer1@users.noreply.github.com>
2026-02-18 17:05:12 +0100

    Rendre la navigation du menu Film opérationnelle

--- assets/js/film_selection.js
+++ assets/js/film_selection.js
@@ -1,1 +1,27 @@
-/*fichier JS de film_selection.html*/
+const CLE_MODE_FILM = "film_mode";
+const MODE_DEFAUT = "battle_royal";
+
+const configurationModes = {
+    battle_royal: {
+        titre: "Battle Royal",
+        pageDemarrer: "../pages/film_battle_royal.html"
+    },
+    classement: {
+        titre: "Classement",
+        pageDemarrer: "../pages/film_consulter_classement.html"
+    }
+};
+
+const modeSauvegarde = sessionStorage.getItem(CLE_MODE_FILM);
+const modeActuel = configurationModes[modeSauvegarde] ? modeSauvegarde : MODE_DEFAUT;
+const configuration = configurationModes[modeActuel];
+
+const titre = document.querySelector("h1");
+if (titre) {
+    titre.textContent = configuration.titre;
+}
+
+const boutonDemarrer = document.getElementById("film-start-button");
+if (boutonDemarrer) {
+    boutonDemarrer.dataset.page = configuration.pageDemarrer;
+}
